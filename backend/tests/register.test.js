431b7b17f2e99072d5c1a2dcdf399c5b51a6f187
DolfShungube <dolfshungube@gmail.com>
2025-04-14 01:55:45 +0200

    t

--- backend/tests/register.test.js
+++ backend/tests/register.test.js
@@ -1,1 +1,41 @@
+import {existingEmail,issueTracker,registrationDetailsPresent} from '../login/utils'
+
+
+test("checking if we recived an existing email from the server",()=>{
+    const result = existingEmail("dolphnoble@gmail.com");
+    expect(result).toBe(false);
+});
+
+test("email does not exist in database",()=>{
+    const result = existingEmail("");
+    expect(result).toBe(true);
+});
+
+
+
+test("if all  details  not present",()=>{
+    const result = registrationDetailsPresent({userType:"",email:"",password:""});
+    expect(result).toBe(false);
+});
+
+
+test("if all  details  not present",()=>{
+    const result = registrationDetailsPresent({userType:"user",email:"",password:"password"});
+    expect(result).toBe(false);
+});
+
+
+
+
+
+test("if user details  present",()=>{
+    const result = registrationDetailsPresent({userType:"user",email:"email",password:"password"});
+    expect(result).toBe(true);
+});
+
+
+
+
+
+
 
